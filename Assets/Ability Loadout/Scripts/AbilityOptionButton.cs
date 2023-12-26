f659c308359325e55c8ace6c68dceae37c14222d
AquaHob <jannes.habeck@gmail.com>
2023-12-26 11:34:44 +0100

    Added the small stuff i did on laptop

--- Assets/Ability Loadout/Scripts/AbilityOptionButton.cs
+++ Assets/Ability Loadout/Scripts/AbilityOptionButton.cs
@@ -6,9 +6,19 @@ public class AbilityOptionButton : StartMenuButton
 {
     [Header("Ability Option")]
     [SerializeField] private GameObject Lock_GO;
+    [SerializeField] private GameObject Fill_GO;
+    private RectTransform FillRect;
+
+    [SerializeField] private float lockedWidth;
+    [SerializeField] private float unlockedWidth;
 
     public void AbilityLoadoutSetup(){
-        Debug.Log("Todo!");
+        this.FillRect = this.Fill_GO.GetComponent<RectTransform>();
+        this.lockedWidth = this.FillRect.rect.width;
+        this.unlockedWidth = gameObject.GetComponent<RectTransform>().rect.width - 10f;
+        // this.FillRect.sizeDelta = new Vector2(this.lockedWidth, FillRect.rect.height);
+        // this.FillRect.sizeDelta = new Vector2(0f, 0f);
+        this.Lock_GO.SetActive(true);
     }
 
     public void ShowLockedStatus(bool on){
